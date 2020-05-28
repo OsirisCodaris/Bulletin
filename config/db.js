e40c6f1f1d148cd15188258baef907588b4f7030
Osiris <engorongbwabrunel@gmail.com>
2020-05-28 06:40:29 -0400

    gitignore

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 let mysql = require('mysql');
 let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    port: 3308,
-    database: 'bullapp'
+    host: 'mysql1006.mochahost.com',
+    user: 'coda18',
+    password: 'c160916',
+    port: 3306,
+    database: 'coda18_bull'
 });
 connection.connect()
 
